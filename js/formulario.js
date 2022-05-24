60b7ce05086f97a6e7fd19b16d0e4aa68ebe360b
rrubio <ro.rubio95@gmail.com>
2022-05-23 23:43:26 -0300

    fix for validacion de email correcto

--- js/formulario.js
+++ js/formulario.js
@@ -3,6 +3,9 @@ function validacion(){
   var apellido = document.getElementById("apellido");
   var email = document.getElementById("email");
   var mensaje = document.getElementById("mensaje");
+  emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+
 
 	if (nombre.value === "" || nombre.value === null) {
 		nombre.setCustomValidity("Ingrese su nombre");
@@ -10,19 +13,19 @@ function validacion(){
 		nombre.setCustomValidity(""); 
 	}
 
-	if (apellido.value == "" || apellido.value === null) {
+	if (apellido.value === "" || apellido.value === null) {
 		apellido.setCustomValidity("Ingrese su apellido");
 	} else {
 		apellido.setCustomValidity(""); 
 	}
 
-  if (email.value == "" || email.value === null) {
-		email.setCustomValidity("Ingrese su email");
+  if (email.value === "" || email.value === null || !emailRegex.test(email.value)) {
+		email.setCustomValidity("Ingrese un email válido");
 	} else {
 		email.setCustomValidity(""); 
 	}
 
-  if (mensaje.value == "" || mensaje.value === null) {
+  if (mensaje.value === "" || mensaje.value === null) {
 		mensaje.setCustomValidity("Ingrese su mensaje");
 	} else {
 		mensaje.setCustomValidity(""); 
